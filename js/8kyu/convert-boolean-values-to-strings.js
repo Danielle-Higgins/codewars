c9a92518b8f1f780327fafb67246783d2165845a
Danielle Higgins <higdan4024@gmail.com>
2024-10-25 23:39:05 -0400

    convert bool to string

---
+++ js/8kyu/convert-boolean-values-to-strings.js
@@ -0,0 +1,9 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// My solution 1
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
+
+// My solution 2
+const boolToWord = (bool) => (bool ? "Yes" : "No");
